2a1c8803fabb5dc5376c7cc4150108386dec4833
Vitali Makhatadze <vitomaxatadze@gmail.com>
2020-02-26 15:43:18 +0400

    Create Longest Word function which return longest word

---
+++ lognestWord/app.js
@@ -0,0 +1,14 @@
+let longestWord = (str) => {
+    let words = str.split(" ");
+    let longestWord = '';
+
+    for(let word of words) {
+       if(word.length>longestWord.length) {
+           longestWord = word
+       }
+    }
+
+    return longestWord;
+}
+
+console.log(longestWord("tes test t test1111"))
