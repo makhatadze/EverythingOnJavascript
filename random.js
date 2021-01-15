1024122a3414058fac50e0c7a27cb07b81684034
makhatadze <vitomaxatadze@gmail.com>
2021-01-15 15:19:27 +0400

    Get a random item from an array

--- random.js
+++ random.js
@@ -2,3 +2,12 @@ let getRandomNumber = (min, max) => {
      return Math.round(Math.random() * (max - min) + min);
  } 
 console.log(getRandomNumber(0, 100));
+
+let names= ['Evelyn', 'Harper', 'Jackson', 'Avery', 'Jack', 'Eleanor', 'Carter'];
+const chooseName = () =>{
+ let randomName = 
+  names[Math.floor(Math.random()
+  *names.length)];
+ return randomName;
+}
+console.log('Random name:', chooseName(names));
